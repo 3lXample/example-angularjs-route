015ea6f73d4529cff06115b84f60d0b95c42c9aa
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2017-08-21 10:28:12 +0700

    Add footer content

--- src/_app/component.js
+++ src/_app/component.js
@@ -11,4 +11,17 @@
   var name   = '3XApp';
   var module = angular.module(name);
 
+  // component for app footer
+  module.component('appFooter', {
+    template:
+      // inline template
+      // we can also use templateUrl for external template
+      // ex: `templateUrl: 'template.html'`
+      // templateUrl will be relative from current page url if not use absolute url path
+      '<footer>' +
+      '  <p>Copyright (c) 2017 3lXample (https://github.com/3lXample)</p>' +
+      '  <p>Designed and built by Anisetus Elly Efendi</p>' +
+      '</footer>'
+  });
+
 })();
