24c8a39e9e8748e18ceef3ebe25c45acc87a29db
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2017-08-21 10:30:48 +0700

    Add header content

--- src/_app/component.js
+++ src/_app/component.js
@@ -11,6 +11,29 @@
   var name   = '3XApp';
   var module = angular.module(name);
 
+  // component for app header
+  module.component('appHeader', {
+    template:
+      // inline template
+      // we can also use templateUrl for external template
+      // ex: `templateUrl: 'template.html'`
+      // templateUrl will be relative from current page url if not use absolute url path
+      '<header>' +
+      '  <div class="row">' +
+      '    <div class="col col-auto">' +
+      '      <img src="{{$ctrl.logoImgUrl}}" width="160" height="44" alt="logo">' +
+      '    </div>' +
+      '    <div class="col">' +
+      '      <h1>Example AngularJS</h1>' +
+      '    </div>' +
+      '  </div>' +
+      '</header>',
+    bindings: {
+      // Should be bind from parent
+      logoImgUrl: '<'
+    }
+  });
+
   // component for app footer
   module.component('appFooter', {
     template:
--- src/_app/controller.js
+++ src/_app/controller.js
@@ -11,4 +11,15 @@
   var name   = '3XApp';
   var module = angular.module(name);
 
+  // Inject dependencies
+  AppController.$inject = ['$scope'];
+
+  // Add app controller
+  module.controller('appController', AppController);
+
+  // AppController Class
+  function AppController($scope) {
+    $scope.headerLogoImgUrl = '_assets/3lXample_logo-160x44.png';
+  }
+
 })();
