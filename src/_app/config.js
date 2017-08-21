4dadea6d89f4343b3a951ebc9cd9559dabb657d2
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2017-08-21 10:11:15 +0700

    Add AngularJS Skeleton

---
+++ src/_app/config.js
@@ -0,0 +1,14 @@
+/**
+ * example-angularjs-route
+ * https://github.com/3lXample/example-angularjs-route
+ *
+ * Copyright (c) 2017 3lXample (https://github.com/3lXample)
+ * Licensed under the MIT license
+ */
+(function() {
+  'use strict';
+
+  var name   = '3XApp';
+  var module = angular.module(name);
+
+})();
