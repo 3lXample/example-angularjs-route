7d18f1c4bcf79747f711cab0e1cd834ced98cb3c
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2017-08-21 10:14:42 +0700

    Add example module

---
+++ src/example/module.js
@@ -0,0 +1,15 @@
+/**
+ * example-angularjs-route
+ * https://github.com/3lXample/example-angularjs-route
+ *
+ * Copyright (c) 2017 3lXample (https://github.com/3lXample)
+ * Licensed under the MIT license
+ */
+(function() {
+  'use strict';
+
+  var name         = '3XApp-example';
+  var dependencies = [];
+  var module       = angular.module(name, dependencies);
+
+})();
